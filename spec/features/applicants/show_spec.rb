cab8799032404fbb7c383e1f13e25ca910afeac2
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-10-21 07:57:49 -0700

    Update us1 test suite

--- spec/features/applicants/show_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'applicant' do
-    xit 'shows the name of the applicant' do
-        visit '/applicant'
-        expect(page).to have_content('Name of Applicant')
-    end
-end
