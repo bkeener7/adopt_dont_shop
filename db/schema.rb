3780ad354ded812ae5874460251b62f113a24dd2
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-10-22 11:00:37 -0700

    Update schema to move status from pet_applications to applicants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_21_214546) do
+ActiveRecord::Schema.define(version: 2022_10_22_175824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,12 +24,12 @@ ActiveRecord::Schema.define(version: 2022_10_21_214546) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   create_table "pet_applications", force: :cascade do |t|
     t.bigint "applicant_id"
     t.bigint "pet_id"
-    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["applicant_id"], name: "index_pet_applications_on_applicant_id"
