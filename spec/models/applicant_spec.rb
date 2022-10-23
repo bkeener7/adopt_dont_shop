ed415ca5319ec4da1cd5ed540d550627740d4478
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-10-22 23:51:12 -0700

    Add validation tests to applicant_spec file

--- spec/models/applicant_spec.rb
+++ spec/models/applicant_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Applicant, type: :model do
   describe 'relationships' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :city }
+    it { should validate_presence_of :state }
+    it { should validate_presence_of :zipcode }
+    it { should validate_presence_of :status }
     it { should have_many :pet_applications }
     it { should have_many(:pets).through(:pet_applications) }
   end
