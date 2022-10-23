cecacfb6470ed4b77526238cae2ef0341562eb18
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-10-22 23:57:44 -0700

    Pass validation tests in applicant_spec

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,4 +1,9 @@
 class Applicant < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :city
+  validates_presence_of :state
+  validates_presence_of :zipcode
+  validates_presence_of :status
   has_many :pet_applications
   has_many :pets, through: :pet_applications
 end
