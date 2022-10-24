11511d2121e4e77d93326aae9c0aa21c252a91f4
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-10-23 19:54:46 -0700

    Create test for us8, pass us8 test with existing code

--- spec/features/applicants/show_spec.rb
+++ spec/features/applicants/show_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'applicants' do
     @pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: @shelter.id)
     @pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter.id)
     @pet_3 = Pet.create!(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter_id: @shelter.id)
+    @pet_4 = Pet.create!(adoptable: true, age: 1, breed: 'irish wolf hound', name: 'Lobo', shelter_id: @shelter.id)
     @applicant1 = Applicant.create!(name: 'Diana Prince', address: '5 Champ de Mars Ave', city: 'Denver', state: 'Colorado', zipcode: 80202, description: 'I love dogs!', status: 'In Progress')
     @applicant2 = Applicant.create!(name: 'Bruce Wayne', address: '1007 Mountain Drive', city: 'Gotham', state: 'New Jersey', zipcode: 07105, description: 'I love bats(and dogs)!', status: 'In Progress')
     @applicant3 = Applicant.create!(name: 'Hughie Campbell', address: '175 Vought Ave', city: 'New York City', state: 'New York', zipcode: 10282, description: 'Cats rule!', status: 'Pending')
@@ -93,5 +94,13 @@ RSpec.describe 'applicants' do
       expect(page).to_not have_button('Submit Application')
     end
 
+    it 'returns partial matches on pet names search' do
+      visit "/applicants/#{@applicant2.id}"
+      fill_in('Search', with: 'Lob')
+      click_on('Search')
+
+      expect(page).to have_content('Lobster')
+      expect(page).to have_content('Lobo')
+    end
   end
 end
