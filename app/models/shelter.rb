c81cdf8b3f3043ab2398ca429aa34ff39176445a
bkeener <bkeener7@gmail.com>
2022-10-24 11:44:42 -0600

    Add reverse sort functionality for admin_shelters index page

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,7 +2,6 @@ class Shelter < ApplicationRecord
   validates :name, presence: true
   validates :rank, presence: true, numericality: true
   validates :city, presence: true
-
   has_many :pets, dependent: :destroy
 
   def self.order_by_recently_created
@@ -10,10 +9,14 @@ class Shelter < ApplicationRecord
   end
 
   def self.order_by_number_of_pets
-    select("shelters.*, count(pets.id) AS pets_count")
-      .joins("LEFT OUTER JOIN pets ON pets.shelter_id = shelters.id")
-      .group("shelters.id")
-      .order("pets_count DESC")
+    select('shelters.*, count(pets.id) AS pets_count')
+      .joins('LEFT OUTER JOIN pets ON pets.shelter_id = shelters.id')
+      .group('shelters.id')
+      .order('pets_count DESC')
+  end
+
+  def self.order_reverse_name
+    find_by_sql('SELECT * FROM shelters ORDER BY shelters.name desc')
   end
 
   def pet_count
